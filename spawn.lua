58b09054637f1202e3f1df807d7d9c778a00f2cb
Ivan Kostrubin <7spawn@gmail.com>
2023-09-01 23:42:58 +0300

    cleared

--- spawn.lua
+++ spawn.lua
@@ -7,10 +7,7 @@ x,y=playerMouse(me)
 obj = createObject('ForceWand',x,y)
 itemEnchants(obj, "", "", "ContinualReplenishment4", "")
 
-
---Рекция на нажатия игрока, например клик мыши (правый, это будет c==2). В pl игрок, а в targ что у него мышкой выделенно.
-
--- Имена зачарований ПРЕДМЕТОВ (enchantments) Nox берет из файлика modifier.db (который я прикрепил к сообщению)
+-- Имена зачарований ПРЕДМЕТОВ (enchantments) Nox берет из файлика modifier.db 
 -- Смотри секцию ENCHANTMENT.
 -- Зачаровать меч на огонь 4-го уровня можно командой
 -- itemEnchants(obj, "", "", "Fire4", "")
